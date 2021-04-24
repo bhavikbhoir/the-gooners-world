55aca0e665ae44ce067ea01938200af2c01deb5a
Bhavik Bhoir <bhvkbhoir95@gmail.com>
2021-04-23 23:33:22 -0500

    Update April 23
    1. Match reports for ARSFUL & ARSEVE updated.
    2. Breaking News - Super League report
    3. Tweet Embed package added
    4. Styles updated to make it compatible on medium screens (iPad screens)

--- src/components/Breaking.js
+++ src/components/Breaking.js
@@ -2,33 +2,54 @@ import React, { Component } from 'react'
 import ReactPlayer from "react-player";
 import {FaHandPointRight, FaHeart, FaHourglass, FaHourglassEnd, FaPen} from 'react-icons/fa'
 import { Row, Col, Card, CardColumns } from 'react-bootstrap'
-import News2 from './News/News2';
-
+import TweetEmbed from 'react-tweet-embed'
 export default class Breaking extends Component {
     render() {
         return (
             <div className="breaking">
                 <h3>Breaking 🔥</h3>
-                <h5 id="kit" style={{marginBottom:"1rem"}}><b>#WelcomeØdegaard</b></h5>
+                <h5 id="kit" style={{marginBottom:"1rem"}}><b>Super Drama!😱 #NoSuperLeague</b></h5>
                 <Row>                  
-                    <Col lg={7} md={7} sm={12}>
-                    <ReactPlayer
+                    <Col lg={5} md={12} sm={12} className="pt-0 pr-1 pl-1 breaking-asset">
+                    {/* <ReactPlayer
                         style={{width: "auto"}}
                         url="https://youtu.be/Gog2TKtBmQE"
-                    />
+                    /> */}
+                    <TweetEmbed id="1384626900056805379"/>
                     </Col>
-                    <Col lg={5} md={5} sm={12}>
+                    <Col lg={7} md={12} sm={12} className="breaking-content">
                         <p style={{textAlign: "left", fontSize: "1.125rem"}}>
-                            <b>Ødegaard is a Gunner!</b>
+                            {/* <b>The Super League Drama</b> */}
                             
-                            <br/>
-                            Norwegian international Martin Odegaard is joining us from Real Madrid on loan for the remainder of the season.
-                            <br/>
-                            <br/>
-                            Mikel Arteta said: “It’s great that we’ve secured Martin to come to us until the end of the season. Martin is of course a player that we all know very well and although still young, he has been playing at the top level for a while. Martin will provide us with quality offensive options and we’re all excited to be integrating him into our plans between now and May.”
+                            {/* <br/> */}
+                            April 19-20, 2021 will probably go down as the most dramatic 48 hours of Arsenal History and Football History in general.
                             <br/>
                             <br/>
-                            Martin will wear the number 11 shirt.
+                            It all started when Arsenal announced that they'll be one of the founding members of the new breakaway European Super league along with the
+                            other top clubs from Europe including Chelsea, Manchester City, Manchester United, Liverpool, Tottenham, Barcelona, Real Madrid, Atletico Madrid, Juventus, Inter Milan, and AC Milan 
+                            with 3 more clubs joining them as founders soon. The remaining 5 spots will be taken by teams passing the qualification matches to make up the 20 teams for the Super League.
+                            <br/>
+                            <br/>
+                            This News didn't go down well with the Fans, players, excluded clubs, and the officials of UEFA and FIFA who went on to declare that the teams participating in this breakaway league will be banned from the competitions such as the World Cup, Euros, UCL, and UEL.
+                            <br/>
+                            <br/>
+                            The fans, players, and managers expressed their disappointment in their respective club's decision. 
+                            Hector Bellerin shared the famous Arsene Wenger farewell message - "To all the Arsenal lovers take care of the values of the club. My love and support forever."
+                            <br/>
+                            <br/>
+                            Arsene Wenger himself expressed his thoughts on this league formation - 
+                            “I would say it's a bad idea. Football has to stay united, it’s the most important thing.
+
+                            It’s based on sporting merit and to respect the history that has been built from European football.
+
+                            I believe, personally, that this idea will not go far." [TalkSport]
+                            <br/>
+                            <br/>
+                            24 hours later, it seemed that the league was in trouble given the circumstances and actions taken by the fans, FA, UEFA, and other official parties.
+                            Chelsea was the first club to withdraw their participation which was followed by Arsenal and other English clubs. Barcelona, Real Madrid, and Juventus have still maintained their participation in this project but it seems to be unlikely that this league will go ahead.
+                            <br/>
+                            <br/>
+                            In the end, it's all good in the hood but Arsenal's struggles on the pitch continue with a disappointing defeat against Everton. With the Europa league looming over us, it seems to be our only hope of salvaging this season and cementing a spot in next year's Champions League.
                         </p>
                     </Col>  
                 </Row>                    
--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import ReactPlayer from "react-player";
 import {FaHeart, FaInfo, FaInfoCircle, FaPlay} from 'react-icons/fa'
 import { Row, Col, Card, CardColumns, Button } from 'react-bootstrap'
-import SLVARS from '../assets/SLVARS.jpg';
+import ARSEVE from '../assets/ARSEVE.jpg';
 
 export default class Fixtures extends Component {
     btnClick() {
-        window.open("https://youtu.be/TKH8sPIN970");
+        window.open("https://youtu.be/Yo0fMPPMhz8");
     }
     detailsbtnClick() {
         window.open("https://www.fctables.com/teams/arsenal-180231/");
@@ -16,10 +16,10 @@ export default class Fixtures extends Component {
             <div className="fixtures">
                 <h3>Fixtures & Results 🏟️</h3>
                 <Row id="large-fixtures">
-                    <Col lg={6} md={12} sm={12}>
+                    <Col lg={6} md={12} sm={12} className="last-match">
                     <iframe frameborder="0"  scrolling="no" width="500" height="200" src="https://www.fctables.com/teams/arsenal-180231/iframe/?type=team-last-match&lang_id=2&country=67&template=12&team=180231&timezone=America/New_York&time=24&width=500&height=200&font=Helvetica&fs=14&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=30&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=FFFFFF&hfc=000000"></iframe>               
                     </Col>
-                    <Col lg={6} md={12} sm={12} style={{textAlign: "right"}}>
+                    <Col lg={6} md={12} sm={12} className="next-match">
                     <iframe frameborder="0"  scrolling="no" width="500" height="200" src="https://www.fctables.com/teams/arsenal-180231/iframe/?type=team-next-match&lang_id=2&country=67&template=12&team=180231&timezone=America/New_York&time=24&width=500&height=200&font=Helvetica&fs=14&lh=22&bg=FFFFFF&fc=333333&logo=1&tlink=1&scfs=30&scfc=333333&scb=1&sclg=1&teamls=80&sh=1&hfb=1&hbc=FFFFFF&hfc=000000"></iframe>
                     </Col>
                     <Col className="details">
@@ -40,27 +40,27 @@ export default class Fixtures extends Component {
 
                 <Row> 
                     <Col lg={6} md={12} sm={12}>
-                        <h5 style={{marginBottom:"1rem"}}><b>Hello Semi Finals 🔴⚪️</b></h5>
+                        <h5 style={{marginBottom:"1rem"}}><b>Disappointment against the Toffees 🔴⚪️</b></h5>
                         <p style={{textAlign: "left", fontSize: "1.125rem"}}>
-                            <b>SLAVIA PRAGUE 0 - 4 ARSENAL (1-5 AGG)</b>
+                            <b>ARSENAL 0 - 1 EVERTON</b>
                             <br/>
                             <br/>
-                            A mesmeric spell of three goals in six first-half minutes laid the platform for a commanding victory in Prague, and sent us through to the semi-final of the Europa League once again.
+                            A second half Bernd Leno own goal condemned us to a home defeat against Everton on Friday night.
                             <br/>
                             <br/>
-                            After a quiet opening 15 minutes, we stepped up through the gears to rip Slavia apart on their own turf, making the disappointment of last week's late equaliser in the first leg at the Emirates a distant memory.
+                            Mikel Arteta made six changes from the side that drew with Fulham last time out, the most notable of which saw Eddie Nketiah lead the line with both Alex Lacazette and Pierre-Emerick Aubameyang ruled out.
                             <br/>
                             <br/> 
-                            Nicolas Pepe, Alexandre Lacazette and Bukayo Saka all found the net in the first-half flurry, and Lacazette added extra gloss to the evening late on.
+                            In the second half, we’d been awarded a penalty when Dani Ceballos was tripped in the box by Richarlison, but Nicolas Pepe was adjudged to be millimetres offside and it was chalked off by VAR.
                             <br/>
                             <br/>                         
-                            We are through to the last four of the Europa League for the third time in the past four seasons, where we will face Villarreal over two legs.
+                            With less than 15 minutes remaining, the visitors hit us on the break as Richarlison got past Granit Xhaka in the box and hit a low cross that hit Leno and went through his legs and in.
                             {/* <br/>
                             <br/>   
                             But the rest of the game was essentially about our efforts to find an equaliser and ended in a defeat. */}
                             <br/>
                             <br/>
-                            <i>We host Fulham in our next Premier League clash.</i>
+                            <i>We play Villarreal next in the first leg of our Europa League clash.</i>
                         </p>
                     </Col>
                     <Col lg={6} md={12} sm={12} id="celebrate">
@@ -68,8 +68,8 @@ export default class Fixtures extends Component {
                         style={{width: "auto"}}
                         url="https://www.youtube.com/watch?v=0sbBijhhvQ0?autoplay=1"
                     /> */}
-                    <img src={SLVARS} alt="Gunners celebrate their dominating victory against Slavia Prague."/>
-                    <caption>Gunners celebrate their dominating victory against Slavia Prague. (Source: Arsenal.com)</caption>
+                    <img src={ARSEVE} alt="Rob Holding in action against Everton."/>
+                    <caption>Rob Holding in action against Everton. (Source: Arsenal.com)</caption>
                     </Col>
                     <Col className="highlights">                    
                     <Button variant="danger" onClick={this.btnClick}><FaPlay /> Watch Highlights</Button>
